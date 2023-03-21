4347ad7643b950084dd467a9ef9da7aa43fbd2ac
thirumaladevika <thirumaladevikathirumaladevika@gmail.com>
2023-03-21 14:14:05 +0530

    ok

--- react28/src/App.js
+++ react28/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react'
+
 import Fun from "./Component/Fun"
 import Cls from "./Component/Cls"
+import Txt from "./Component/Txt"
 function App(){
     return <>
+    
         <Cls />
         <Fun />
-   <h1>GM</h1> ,
+        <Txt />
+   <div style={{margin:"10px"}}>
+   <h1 style={{color:"white",backgroundColor:"red",textAlign:"center",padding:"50px"}}>Hello World !!</h1> ,
+   </div>
     <hr />
     </>
     
---
+++ react28/src/Component/Txt.jsx
@@ -0,0 +1,7 @@
+let Fun=()=> {
+    let P_name="iphone 14";
+return <div>
+    <p>Devika</p>
+</div>
+}
+export default Fun
