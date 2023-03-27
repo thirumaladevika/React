45b0c4f3482d281bcdbcbba822b6e654f01c1244
thirumaladevika <thirumaladevikathirumaladevika@gmail.com>
2023-03-27 12:18:54 +0530

    ok

--- react28/src/App.js
+++ react28/src/App.js
@@ -1,3 +1,50 @@
+//Ex useEffect{} using react
+import React,{useState,useEffect} from 'react'
+function App(){
+  const[count,setCount]=useState(1);
+  useEffect(()=>console.log(count),[count])
+  return(
+    <>
+    <p>You Clicked {count} Number of Time</p>
+    <button onClick={()=>setCount(count+1)}>Click Me</button>
+    </>
+  )
+}
+export default App
+/*use state in React
+ex1:import React, { useState } from 'react'
+
+const App = () => {
+  const [count,setcount]=useState(0);
+  return (
+    <div>
+      <center>
+        <h1>{count}</h1>
+        <button onClick={()=>setcount(count+1)}>change</button>
+      </center>
+    </div>
+  )
+}
+export default App*/
+/*ex2 import React, { useState } from 'react'
+
+const App = () => {
+  const [name,setname]=useState("Devika");
+  return (
+    <div>
+      <center>
+        <h1>{name}</h1>
+        <button onClick={()=>setname("Thirumala devika")}>change</button>
+      </center>
+    </div>
+  )
+}
+
+export default App*/
+
+
+/*
+inline styling
 import React from 'react'
 
 import Fun from "./Component/Fun"
@@ -16,4 +63,16 @@ function App(){
     </>
     
 }
-export default App
+export default App*/
+/*onclick Event handler
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <button onClick={()=>console.log("clicked")}>Click here</button>
+    </div>
+  )
+}
+
+export default App*/
